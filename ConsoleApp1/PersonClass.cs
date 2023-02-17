7d990a3e3e37d8f78299f56d03d6c89841ca4074
uday99 <udaypeddu9999@gmail.com>
2023-02-17 10:32:28 -0600

    test

---
+++ ConsoleApp1/PersonClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PersonClass
+    {
+        private string name;
+        public void getName()
+        {
+            Console.WriteLine("Hi");
+        }
+        public void getName(string name)
+        {
+            Console.WriteLine("H");
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            b n = new b();
+            n.n();
+            PersonClass p = new PersonClass();
+            p.getName("h");
+        }
+
+        class b : MmClass
+        {
+            public override void n()
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
